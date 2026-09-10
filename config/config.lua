f6a2fb4b47172355d4822ddf97be9e9b9405629e
vuta50828-cmd <vuta50828@gmail.com>
2026-09-10 12:31:38 +0700

    Create config.lua

---
+++ config/config.lua
@@ -0,0 +1,22 @@
+local Config = {}
+
+Config.Name = "REDMITRIX"
+Config.Version = "1.0.0"
+
+Config.UI = {
+	Theme = "RED",
+	Mobile = true,
+	DraggableIcon = true,
+	MinimizeButton = true,
+	CloseButton = true
+}
+
+Config.Features = {
+	BootFPS = true,
+	FixCrack = true,
+	AntiAFK = true,
+	Graphics = true,
+	ShowFPS = true
+}
+
+return Config
